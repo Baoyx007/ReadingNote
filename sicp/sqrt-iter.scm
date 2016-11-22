a9c26b1aadd742d69cb296c84dc8b71768f71cc8
haven.bao <baoyx007@gmail.com>
2016-11-22 16:57:53 +0800

    finish pratice 1.7-1.8

--- sicp/sqrt-iter.scm
+++ sicp/sqrt-iter.scm
@@ -1,5 +1,5 @@
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+  (if (good-enough2? guess x)
       guess
       (sqrt-iter (imporve guess x) x )
     ))
@@ -14,5 +14,23 @@
   (< (abs (- (* guess guess) x )) 0.001)
 )
 
+(define (good-enough2? guess x)
+  (< (abs (- (imporve guess x) guess))
+    (* guess 0.001))
+)
+
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+(define (cube-root-iter guess x)
+  (if (good-enough2? guess x)
+      guess
+      (cube-root-iter  (imporve-cube guess x) x )
+    ))
+
+(define (imporve-cube guess x)
+        (/ (+ (/ x (* guess guess)) (* 2 guess)) 3)
+  )
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
